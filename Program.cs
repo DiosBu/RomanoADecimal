0dbdfd4cd42116c5b2fd13fc1f448ad06200baae
William Alexander Rodriguez Valentin <william_rodriguez@ucne.edu.do>
2025-09-02 01:16:23 -0400

    condisiones para las restricciones de las letras

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace RomanoADecimal
             Console.Write("Ingrese un numero romano: ");
             string romano = Console.ReadLine().ToUpper();
 
-            int decimalNum = convertores(romano);
+            int decimalNum = ConvertirRomanos(romano);
 
             if (decimalNum != -1)
                 Console.WriteLine($"El numero romano {romano} equivale a {decimalNum} en decimal.");
@@ -19,38 +19,62 @@ namespace RomanoADecimal
                 Console.WriteLine("Numero romano invalido.");
         }
 
-        static int convertores(string romano)
+        static int ConvertirRomanos(string romano)
         {
             Dictionary<char, int> valores = new Dictionary<char, int>()
             {
-
-                {'I', 1 },
-                {'V', 5 },
-                {'X', 10 },
-                {'L', 50 },
-                {'C', 100 },
-                {'D', 500 },
-                {'M', 1000 }
+                {'I', 1 }, {'V', 5 }, {'X', 10 }, {'L', 50 }, {'C', 100 }, {'D', 500 }, {'M', 1000 }
             };
 
             int total = 0;
             int valorAnterior = 0;
+            int repeticiones = 1;
+            char simboloAnterior = '\0';
 
             for (int i = romano.Length - 1; i >= 0; i--)
             {
                 char letra = romano[i];
                 if (!valores.ContainsKey(letra))
-                    return -1; 
+                    return -1;
+
                 int valor = valores[letra];
 
-                if (valor < valorAnterior)
-                    total -= valor;
+                if (letra == simboloAnterior)
+                {
+                    repeticiones++;
+                    if ((letra == 'V' || letra == 'L' || letra == 'D') && repeticiones > 1)
+                        return -1;
+                    if (repeticiones > 3)
+                        return -1;
+                }
                 else
-                    total += valor;
+                {
+                    repeticiones = 1;
+                }
 
+                if (valor < valorAnterior)
+                {
+                    if (!EsSustraccionValida(letra, simboloAnterior))
+                        return -1;
+                    total -= valor;
+                }
+                else
+                {
+                    total += valor;
+                }
+
+                simboloAnterior = letra;
                 valorAnterior = valor;
             }
+
             return total;
+
+
+            static bool EsSustraccionValida(char menor, char mayor)
+            {
+                return (menor == 'I' && (mayor == 'V' || mayor == 'X')) || (menor == 'X' && (mayor == 'L' || mayor == 'C')) || (menor == 'C' && (mayor == 'D' || mayor == 'M'));
+            }
+
         }
     }
 }
